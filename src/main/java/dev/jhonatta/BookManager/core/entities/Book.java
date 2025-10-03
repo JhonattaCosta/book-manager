61fdb0ad248e6a19b86ec5380a67f5496d8daaee
Jhonatta Costa <jhonattasantos27@gmail.com>
2025-10-03 06:48:44 -0300

    feat: add Book Entity

---
+++ src/main/java/dev/jhonatta/BookManager/core/entities/Book.java
@@ -0,0 +1,7 @@
+package dev.jhonatta.BookManager.core.entities;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record Book(Long id, String name, String description, LocalDateTime releaseDay, String identifier, List<Category> categories) {
+}
