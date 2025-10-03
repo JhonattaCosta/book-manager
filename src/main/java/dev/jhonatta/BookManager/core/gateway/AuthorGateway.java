5d941f7f765cb653630ac00e7e3c79b47c8c7617
Jhonatta Costa <jhonattasantos27@gmail.com>
2025-10-03 07:05:28 -0300

    feat: add Author entity and usecases
    -Add Author reference to Book

---
+++ src/main/java/dev/jhonatta/BookManager/core/gateway/AuthorGateway.java
@@ -0,0 +1,8 @@
+package dev.jhonatta.BookManager.core.gateway;
+
+import dev.jhonatta.BookManager.core.entities.Author;
+
+public interface AuthorGateway {
+
+    Author createAuthor (Author author);
+}
---
+++ src/main/java/dev/jhonatta/BookManager/core/usercase/author/CreateAuthorUseCase.java
@@ -0,0 +1,8 @@
+package dev.jhonatta.BookManager.core.usercase.author;
+
+import dev.jhonatta.BookManager.core.entities.Author;
+
+public interface CreateAuthorUseCase {
+
+    public Author execute(Author author);
+}
